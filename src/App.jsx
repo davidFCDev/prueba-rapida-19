7ea7fd224d1287abe9be313b140d8f49d9339815
David Fernández <davidfernandezcomesana@gmail.com>
2023-08-07 11:47:56 +0200

    search

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Movies } from "./components/Movies";
 import responseMovies from "./mocks/with-results.json";
+import { useId } from "react";
 
 function App() {
+  const [search, updateSearch] = useState("");
+  const [error, setError] = useState(null);
+  const isFirstInput = useId(true);
   const movies = responseMovies.Search;
 
   const mappedMovies = movies.map((movie) => {
@@ -14,14 +19,49 @@ function App() {
     };
   });
 
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === "";
+      return;
+    }
+    if (search === "") {
+      setError("No se puede buscar una película vacía");
+      return;
+    }
+
+    if (search.length < 3) {
+      setError("La búsqueda debe tener al menos 3 caracteres");
+      return;
+    }
+
+    setError(null);
+  }, [search]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Buscando...");
+  };
+
+  const handleChange = (event) => {
+    const newSearch = event.target.value;
+    updateSearch(newSearch);
+  };
+
   return (
     <div className="page">
       <header>
         <h1>Buscador de películas</h1>
-        <form>
-          <input type="text" placeholder="Buscar..." />
+        <form onSubmit={handleSubmit}>
+          <input
+            onChange={handleChange}
+            name="query"
+            value={search}
+            type="text"
+            placeholder="Buscar..."
+          />
           <button type="submit">Buscar</button>
         </form>
+        {error && <p style={{ color: "red" }}>{error}</p>}
       </header>
 
       <main>
