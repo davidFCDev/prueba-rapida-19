300fc5eb93a1b606ed123e94495b8696326cfffd
David Fernández <davidfernandezcomesana@gmail.com>
2023-08-07 12:01:03 +0200

    sorted movies

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ function useSearch() {
 function App() {
   const [sort, setSort] = useState(false);
   const { search, updateSearch, error } = useSearch();
-  const { movies, getMovies, loading } = useMovies({ search });
+  const { movies, getMovies, loading } = useMovies({ search, sort });
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -61,6 +61,7 @@ function App() {
             type="text"
             placeholder="Buscar..."
           />
+          <input type="checkbox" onChange={handleSort} />
           <button>Buscar</button>
         </form>
         {error && <p style={{ color: "red" }}>{error}</p>}
